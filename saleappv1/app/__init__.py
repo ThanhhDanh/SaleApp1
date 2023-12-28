7344a2708ff9bf6837c2b1b8f67a59a18ce2ef76
ThanhhDanh <118066468+ThanhhDanh@users.noreply.github.com>
2023-12-28 22:56:36 +0700

    update

--- saleappv1/app/__init__.py
+++ saleappv1/app/__init__.py
@@ -3,21 +3,12 @@ from urllib.parse import quote
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-
-import App
-
 app = Flask(__name__)
 
-app.secret_key='uhiuyg7yt'
-
-app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:%s@localhost/saleapp1?charset=utf8mb4" % quote('Admin@123')
+app.secret_key = '^%*&^^HJGHJGHJFD%^&%&*^*(^^^&^(*^^$%^GHJFGHJH'
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/saleapp?charset=utf8mb4" % quote('Admin@123')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-app.config['PAGE_SIZE'] = 2
+app.config["PAGE_SIZE"] = 6
 
 db = SQLAlchemy(app=app)
 login = LoginManager(app=app)
-
-
-
-
-
---
+++ saleappv1/app/index.py
@@ -0,0 +1,125 @@
+import math
+
+from flask import render_template, request, redirect, session, jsonify
+import dao
+import utils
+from app import app, login
+from flask_login import login_user
+
+
+@app.route('/')
+def index():
+    kw = request.args.get('kw')
+    cate_id = request.args.get('cate_id')
+    page = request.args.get('page')
+
+    products = dao.load_products(kw, cate_id, page)
+
+    num = dao.count_product()
+
+    return render_template('index.html', products=products,
+                           pages=math.ceil(num/app.config['PAGE_SIZE']))
+
+
+@app.route('/admin/login', methods=['post'])
+def admin_login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    user = dao.auth_user(username=username, password=password)
+    if user:
+        login_user(user=user)
+
+    return redirect('/admin')
+
+
+@app.route('/api/cart', methods=['post'])
+def add_to_cart():
+    """
+    {
+        "cart": {
+            "1": {
+                "id": "1",
+                "name": "ABC",
+                "price": 12,
+                "quantity": 2
+            }, "2": {
+                "id": "2",
+                "name": "ABC",
+                "price": 12,
+                "quantity": 1
+            }
+        }
+    }
+    :return:
+    """
+    data = request.json
+
+    cart = session.get('cart')
+    if cart is None:
+        cart = {}
+
+    id = str(data.get("id"))
+    if id in cart: # sp da co trong gio
+        cart[id]['quantity'] += 1
+    else: # sp chua co trong gio
+        cart[id] = {
+            "id": id,
+            "name": data.get('name'),
+            "price": data.get('price'),
+            "quantity": 1
+        }
+
+    session['cart'] = cart
+
+    return jsonify(utils.count_cart(cart))
+
+
+@app.route('/api/cart/<product_id>', methods=['put'])
+def update_cart(product_id):
+    cart = session.get('cart')
+    if cart and product_id in cart:
+        quantity = request.json.get('quantity')
+        cart[product_id]['quantity'] = int(quantity)
+
+    session['cart'] = cart
+
+    return jsonify(utils.count_cart(cart))
+
+
+@app.route('/api/cart/<product_id>', methods=['delete'])
+def delete_cart(product_id):
+    cart = session.get('cart')
+    if cart and product_id in cart:
+        del cart[product_id]
+
+    session['cart'] = cart
+
+    return jsonify(utils.count_cart(cart))
+
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+
+@app.route("/login")
+def process_user_login():
+    return render_template('login.html')
+
+
+@login.user_loader
+def get_user(user_id):
+    return dao.get_user_by_id(user_id)
+
+
+@app.context_processor
+def common_response():
+    return {
+        'categories': dao.load_categories(),
+        'cart': utils.count_cart(session.get('cart'))
+    }
+
+
+if __name__ == '__main__':
+    from app import admin
+    app.run(debug=True)
